2cad54555094a606cd80c84872dbf84be4024bd9
Codebuster54 <Nicholass077@gmail.com>
2018-02-28 10:51:34 -0500

    1.3.2

---
+++ CS_Files/1.3.2/Nicholas_1_3_2.py
@@ -0,0 +1,26 @@
+def add_tip(total, tip_percent): 
+    ''' Return the total amount including tip'''
+    tip = tip_percent*total
+    return total + tip
+
+def mean(a,b,c):
+    '''returnes the total of three numbers'''
+    numsum=a+b+c
+    total=numsum/3.0
+    return total
+    
+def perimeter(base, height):
+    '''returnes the perimiter of rectangle'''
+    p=(base+height)*2
+    return p 
+    
+def hyp(leg1, leg2):
+    '''returnes Hypotenuse of right triangle'''
+    leg1=leg1**2
+    leg2=leg2**2
+    leg3=(leg1+leg2)**0.5
+    return leg3
+    
+  
+    
+    
